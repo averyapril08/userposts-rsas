74240dab75e28c86c69e63d378351e74e31de75c
Avery Liu <avery@Averys-MacBook-Pro.local>
2019-05-22 14:15:46 -0700

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component{
 
     render(){
         return(
-            <div style={{color:"red" }}>
+            <div style={{color:"red", textAlign:"center" }}>
                 <h1>hello,please search below</h1>
             </div>
         )
